d089e0716ca35e81596c68a917c79845062bfb22
unknown <abilp444@gmail.com>
2023-12-04 02:14:05 +0700

    add new page

--- app/src/main/java/com/project/jobfinderapp/AmbilLowonganActivity.kt
+++ app/src/main/java/com/project/jobfinderapp/AmbilLowonganActivity.kt
@@ -2,6 +2,7 @@ package com.example.massiveproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.jobfinderapp.R
 
 class AmbilLowonganActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

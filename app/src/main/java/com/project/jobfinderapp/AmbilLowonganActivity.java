c76be79edb70996991ccabbc0f2e52f89662e6b5
unknown <abilp444@gmail.com>
2023-12-12 00:49:12 +0700

    Update New Page

--- app/src/main/java/com/project/jobfinderapp/AmbilLowonganActivity.java
+++
@@ -1,6 +0,0 @@
-package com.project.jobfinderapp;
-
-import android.app.Activity;
-
-public class AmbilLowonganActivity extends Activity {
-}
---
+++ app/src/main/java/com/project/jobfinderapp/Notifikasi.kt
@@ -0,0 +1,3 @@
+package com.project.jobfinderapp
+
+data class Notifikasi(val image: Int)
